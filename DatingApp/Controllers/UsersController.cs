e639b837cdb244bacf27849e173b14069041abac
Vignesh <53811429+rkvignesh@users.noreply.github.com>
2021-01-02 16:28:11 +0530

    Add project files.

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext Context)
+        {
+            _context = Context;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("GetUser")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
